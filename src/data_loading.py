f1865acb8c339f379c1be96f6c7b13f1b2f4a6ff
John Miclat <jmiclat@satlantisllc.com>
2023-12-02 17:58:33 -0500

    Remove prints

--- src/data_loading.py
+++ src/data_loading.py
@@ -42,9 +42,6 @@ class FaceDataset(Dataset):
             # Now create the full paths
             image_path = os.path.join(self.image_dir, image_filename)
             mask_path = os.path.join(self.mask_dir, mask_filename)
-            
-            print("image_path: ", image_path)
-            print("mask_path: ", mask_path) 
                        
             image = np.array(Image.open(image_path).convert("RGB")) # Convert to RGB
             mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32) # Convert to grayscale
